57495df66e043ec04a7b0098dbe88e2ccb573d78
Jeff Rowberg <jeff@rowberg.net>
2019-01-23 21:34:59 -0500

    Add documentation to module init files

--- __init__.py
+++ __init__.py
@@ -1,3 +1,12 @@
+"""
+This module provides protocol and packet definitions for communicating with
+newer Robotis Dynamixel servos that implement version 2 of the Dynamixel
+protocol. In addition to the basic protocol, it also abstracts common sets of
+transactions (such as writing new values to control table registers) into simple
+servo class methods, so the application does not require knowledge or use of the
+underlying protocol directly.
+"""
+
 # .py files
 from .protocol import *
 from .control_table import *
